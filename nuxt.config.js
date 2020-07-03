3880906277e2019ef0abb2f3154530ffb2a736d6
csdoker <csd758371536@gmail.com>
2020-07-03 17:18:20 +0800

    将node-sas替换为dart-sass，并添加相关配置

--- nuxt.config.js
+++ nuxt.config.js
@@ -70,6 +70,11 @@ export default {
   ** See https://nuxtjs.org/api/configuration-build/
   */
   build: {
+    loaders: {
+      sass: {
+        implementation: require("sass")
+      }
+    },
     postcss: {
       preset: {
         features: {
